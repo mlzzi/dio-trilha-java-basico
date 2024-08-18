90e63b9301e6dfa29422603dfa31594df5c607a5
mlzzi <muriloluzzi@gmail.com>
2024-08-18 13:36:08 -0300

    Projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ContaTerminal {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Locale;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        //TODO:Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //TODO: as mensagens para o nosso usuário
+        System.out.println("Bem-vindo ao Banco X");
+
+        //TODO: obter pela Scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o número da Conta:");
+        int conta = scanner.nextInt();
+
+        //TODO: obter pela Scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu Nome:");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite o seu Sobrenome:");
+        String sobrenome = scanner.next();
+
+        System.out.println("Por favor, digite seu Saldo:");
+        Double saldo = scanner.nextDouble();
+
+        //TODO: exibir a mensagem conta criada
+        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
